1a8a1b7b1810f6bcbfdd7767d43d2447c2a89cf4
penglifr <penglifr@isoftstone.com>
2024-05-21 17:49:16 +0800

    时间过滤格式

--- src/main/java/com/freezonex/aps/modules/asset/dto/WorkOrderListReq.java
+++ src/main/java/com/freezonex/aps/modules/asset/dto/WorkOrderListReq.java
@@ -1,9 +1,11 @@
 package com.freezonex.aps.modules.asset.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.freezonex.aps.common.api.BasePage;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.ToString;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.util.Date;
 
@@ -22,5 +24,7 @@ public class WorkOrderListReq extends BasePage {
 
     private String orderType;
 
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    @JsonFormat(pattern = "yyyy-MM-dd", timezone = "GMT+8")
     private Date creationTime;
 }
