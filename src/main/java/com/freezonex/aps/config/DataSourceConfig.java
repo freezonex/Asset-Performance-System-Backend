deee19fc155bb6607a91b4f65bd5a9da55fa16e4
Wenhao <yuwenhao@freezonex.io>
2024-05-25 22:05:57 +0800

    数据初始化链接超时问题

---
+++ src/main/java/com/freezonex/aps/config/DataSourceConfig.java
@@ -0,0 +1,74 @@
+package com.freezonex.aps.config;
+
+import com.alibaba.druid.pool.DruidDataSource;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import javax.sql.DataSource;
+
+@Configuration
+public class DataSourceConfig {
+
+    @Value("${spring.datasource.druid.url}")
+    private String dbUrl;
+
+    @Value("${spring.datasource.druid.username}")
+    private String username;
+
+    @Value("${spring.datasource.druid.password}")
+    private String password;
+
+    @Value("${spring.datasource.druid.initial-size}")
+    private int initialSize;
+
+    @Value("${spring.datasource.druid.min-idle}")
+    private int minIdle;
+
+    @Value("${spring.datasource.druid.max-active}")
+    private int maxActive;
+
+    @Value("${spring.datasource.druid.max-wait}")
+    private long maxWait;
+
+    @Value("${spring.datasource.druid.time-between-eviction-runs-millis}")
+    private long timeBetweenEvictionRunsMillis;
+
+    @Value("${spring.datasource.druid.min-evictable-idle-time-millis}")
+    private long minEvictableIdleTimeMillis;
+
+    @Value("${spring.datasource.druid.validation-query}")
+    private String validationQuery;
+
+    @Value("${spring.datasource.druid.validation-query-timeout}")
+    private int validationQueryTimeout;
+
+    @Value("${spring.datasource.druid.remove-abandoned}")
+    private boolean removeAbandoned;
+
+    @Value("${spring.datasource.druid.remove-abandoned-timeout-millis}")
+    private long removeAbandonedTimeoutMillis;
+
+    @Value("${spring.datasource.druid.keep-alive}")
+    private boolean keepAlive;
+
+    @Bean
+    public DataSource dataSource() {
+        DruidDataSource dataSource = new DruidDataSource();
+        dataSource.setUrl(dbUrl);
+        dataSource.setUsername(username);
+        dataSource.setPassword(password);
+        dataSource.setInitialSize(initialSize);
+        dataSource.setMinIdle(minIdle);
+        dataSource.setMaxActive(maxActive);
+        dataSource.setMaxWait(maxWait);
+        dataSource.setTimeBetweenEvictionRunsMillis(timeBetweenEvictionRunsMillis);
+        dataSource.setMinEvictableIdleTimeMillis(minEvictableIdleTimeMillis);
+        dataSource.setValidationQuery(validationQuery);
+        dataSource.setValidationQueryTimeout(validationQueryTimeout);
+        dataSource.setRemoveAbandoned(removeAbandoned);
+        dataSource.setRemoveAbandonedTimeoutMillis(removeAbandonedTimeoutMillis);
+        dataSource.setKeepAlive(keepAlive);
+
+        return dataSource;
+    }
+}
